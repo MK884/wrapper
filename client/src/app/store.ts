15dddf7d5ad62d7f8337cedf23ff05e81882fc39
MK884 <marchantkhalid7810@gmail.com>
2024-09-30 20:38:32 +0530

    redux-persist

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,13 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
+const authPersistConfig = {
+    key: "auth",
+    storage,
+  };
+
+  const persistedReducer = persistReducer(authPersistConfig, authReducer);
 
 export const store = configureStore({
     reducer:{
-        auth: authReducer 
+        auth: persistedReducer 
     }
 })
 
+export const persistor = persistStore(store);
 
 // type for useSelecet
 export type RootState = ReturnType<typeof store.getState>;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
-import { store } from './app/store.ts';
+import { persistor, store } from './app/store.ts';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
-        <Provider store={store}>
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
             <App />
-        </Provider>
-
+        </PersistGate>
+    </Provider>
 );
